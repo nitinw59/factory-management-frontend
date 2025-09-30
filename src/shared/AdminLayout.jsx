c2a47a0ad5130fd56db978b6279f579c433acaee
nitin wadhwa <nitinw59@gmail.com>
2025-09-30 15:09:58 +0530

    beta v1.01

--- src/shared/AdminLayout.jsx
+++ src/shared/AdminLayout.jsx
@@ -62,6 +62,8 @@ const AdminLayout = () => {
                  <NavLink to="/admin/inventory" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Trims Dashboard</NavLink>
                  <NavLink to="/admin/trim-items" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Manage Trim Items</NavLink>
                  <NavLink to="/admin/fabric-types" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Manage Fabric Types</NavLink>
+                 <NavLink to="/admin/fabric-colors" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Manage Fabric Colors</NavLink>
+
               </NavDropdown>
               
               <NavDropdown title="Portals">
